d88ed0b7739f7aae3701a42c4f68242e0cbcb35c
gherlea <101740436+gherlea@users.noreply.github.com>
2022-03-24 06:05:43 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,42 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello");
+        System.out.println("Crina");
+        int x = 3;
+        int y = 5;
+        int result = x + y;
+        System.out.println(result);
+        double a = 105;
+        double b = 32;
+        System.out.println(a/b);
+        int c = -5;
+        int d = 8;
+        int e = 6;
+        System.out.println(c+d*e);
+        int f = 55;
+        int g = 9;
+        int h = 9;
+        System.out.println((f+g)%h);
+        double i = 20;
+        double j = -3;
+        double k = 5;
+        double l = 8;
+        System.out.println(i+j*k/l);
+        int m = 5;
+        int n = 15;
+        int o = 3;
+        int p = 2;
+        int r = 8;
+        int s = 3;
+        System.out.println(m+n/o*p-r%3);
+
+
+
+
+
+    }
+}
